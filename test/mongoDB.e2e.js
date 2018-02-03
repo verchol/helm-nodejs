4a807a2fe1a4399a9e9dbf1b724519a484f14861
verchol <verchol@gmail.com>
2018-02-04 01:56:01 +0200

    add test

--- test/mongoDB.e2e.js
+++ test/mongoDB.e2e.js
@@ -1,11 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+describe('test', ()=>{
+
 
 // Connection URL
-const local = "192.169.99.100"
-const url = `mongodb://${proccess.env.MOGNO_HOST||localHost}:27017`;
-
-// Database Name
+const localHost = "localhost"
+const localMongoPort = "5000"
+const url = `mongodb://${process.env.MONGO_HOST||localHost}:${process.env.MONGO_PORT || localMongoPort }`;
+console.log(`url = ${url}`);
 const dbName = 'nodeK8s';
 const insertDocuments = function(db, callback) {
   // Get the documents collection
@@ -15,20 +17,27 @@ const insertDocuments = function(db, callback) {
     {date : new Date}, {version : process.envVersion}
   ], function(err, result) {
     assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
+    console.log(JSON.stringify(result));
+    assert.equal(2, result.result.n);
+    assert.equal(2, result.ops.length);
     console.log("Inserted 3 documents into the collection");
     callback(result);
   });
 }
-// Use connect method to connect to the server
+
+// Database Name
+it('connect to DB',  (done)=>{
+
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
 
-  insertDocuments(db, function() {
+  insertDocuments(db,  ()=> {
     client.close();
-  });
+    done();
+  })
+});
+})
 });
